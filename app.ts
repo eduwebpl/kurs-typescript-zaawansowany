fce3712776f06cdee5385d45ed009991de71622a
Adam Gospodarczyk <adam@overment.com>
2020-06-10 15:21:16 +0200

    07-finish

--- app.ts
+++ app.ts
@@ -1,29 +1,13 @@
-interface Length {
-  length: number
+// Problem: Uppercase string or array of strings
+
+function upperAnything(data: string | string[]): string | string[] {
+  if (typeof data === "string") {
+    return data.toUpperCase();
+  }
+
+  return data.map(item => item.toUpperCase());
 }
 
-interface Filterable {
-  filter: Function
-}
+upperAnything(['eduweb', 'overment']) /*?*/
+upperAnything('Adam') /*?*/
 
-function howLong<T extends Length & Filterable>(data: T): number {
-  return data.length;
-}
-
-howLong([1, 2, 3]) /*?*/
-howLong("Abc") /*?*/
-
-
-class Song {
-  constructor(public title: string, public year: number) {}
-}
-
-function getInfo<T extends Song>(song: T): string {
-  return `${song.title} @ ${song.year}`;
-}
-
-const song = new Song("We Will Rock You", 1977);
-getInfo(song) /*?*/
-
-const movie = { title: 'Gone In 60 Seconds', year: 2000 }
-getInfo(movie) /*?*/
