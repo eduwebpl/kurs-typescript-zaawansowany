b54f4760b2ec02f2bb2b95159fcb09b72d44a41c
Adam Gospodarczyk <adam@overment.com>
2020-06-24 19:47:20 +0200

    10-finish

---
+++ app.ts
@@ -0,0 +1,30 @@
+let a: object // arrays and objects 
+let b: Object
+let c: {}
+
+typeof '' /*?*/
+// // b = '123'
+// // b = 1234
+// b = false
+// // b.
+
+// interface Foo { 
+//   foo: string
+// }
+
+// const obj = { // : Foo | : Object
+//   foo: 'text' 
+// }
+
+// // obj.foo;
+
+// const bar: Object = {};
+// bar.
+
+// const textObj: Object = '';
+
+// Object.create({})
+// // Object.create(null);
+// // Object.create(undefined);
+// // Object.create(10);
+// // Object.create('');
