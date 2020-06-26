1c5d7639be664513eef77a20a9342c18fed006b1
Adam Gospodarczyk <adam@overment.com>
2020-06-26 07:32:42 +0200

    13-finish

--- app.ts
+++ app.ts
@@ -7,12 +7,27 @@ class Playlist {
 }
 
 function play(media: Song | Playlist) {
-  if ('title' in media) {
-    return 'Play song!'
+  if (media instanceof Song) {
+    return media.title;
   }
 
-  return 'Play playlist.'
+  return media.name;
 }
 
-const bestThemeEver = new Song('Game of Thrones Theme');
-play(bestThemeEver); /*?*/
+const bestThemeEver = new Playlist('Game of Thrones Theme');
+play(bestThemeEver); /*?*/ 
+
+class Movie {
+  constructor(public title: string) {}
+
+  play() {
+    return this.title
+  }
+}
+
+const media = new Movie('Gone in 60 seconds.');
+
+media instanceof Movie /*?*/
+media instanceof Object /*?*/
+Object.getPrototypeOf(media) === Movie.prototype /*?*/
+media.constructor === Movie; /*?*/
