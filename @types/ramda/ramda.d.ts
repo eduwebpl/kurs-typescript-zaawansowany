2118a5c328f1ff7898c00997ee5fb1e0713d03f6
Adam Gospodarczyk <adam@overment.com>
2020-06-29 16:13:44 +0200

    15-finish

---
+++ @types/ramda/ramda.d.ts
@@ -0,0 +1,5 @@
+import * as ramda from 'ramda';
+
+declare module 'ramda' {
+  // export function add(a: number, b: number): number
+} 
---
+++ app.ts
@@ -0,0 +1,5 @@
+import * as R from 'ramda';
+
+R.add(5, 3);
+
+export function foo(bar: string): void {}
