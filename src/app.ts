7db3b17e10682047c7f42b0a28b85e0f301dffdb
Adam Gospodarczyk <adam@overment.com>
2020-07-10 14:23:56 +0200

    23-finish

--- src/app.ts
+++ src/app.ts
@@ -10,3 +10,7 @@ const getTitle = (song?: Song) => song.title;
 
 // Use prettier
 const abc = 'test';
+
+// Use objects as payloads for functions
+type SendMail = (type: string, from: string, to: string, subject: string) => Boolean;
+type SendMail2 = (type: string, data: { from: string; to: string; subject: string }) => Boolean;
